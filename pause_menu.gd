3a6a5736d58d772b5e29ff7db0575da51eeb112b
Andres Kushnir <andreskushnir@gmail.com>
2026-06-14 23:30:02 +0300

    fix: pause menu logic detached from main_scene and now works correctly

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,10 +1,26 @@
 extends Control
 
 
-func _on_resume_pressed() -> void:
-	get_tree().paused = false
+func _ready() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	visible = false
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle_pause()
+
+
+func _on_resume_pressed() -> void:
+	toggle_pause()
+
 
 func _on_quit_pressed() -> void:
 	# TODO: save, then quit
 	get_tree().quit()
+
+
+func toggle_pause():
+	print(get_tree().get_class())
+	get_tree().paused = !get_tree().paused
+	visible = get_tree().paused
